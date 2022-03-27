090b292e5f7c7d14293a81e6bcf4445033a678e6
Rafael Lefever <rafael.lefever@discomco.pl>
2022-03-27 20:45:04 +0200

    added TestNewIdentityFrom in sdk

--- th-sensor/main.go
+++ th-sensor/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/rgfaber/go-vesca/th-sensor/domain"
 	"os"
 )
@@ -11,7 +12,11 @@ const (
 )
 
 func main() {
-	sensorId = os.Getenv(TH_SENSOR_ID)
+	sensorId := os.Getenv(TH_SENSOR_ID)
 	act := domain.NewActor(sensorId)
-
+	if act == nil {
+		err := fmt.Errorf("Could not create actor.")
+		fmt.Println("Error:", err)
+		os.Exit(-1)
+	}
 }
